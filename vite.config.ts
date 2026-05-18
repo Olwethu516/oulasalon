97edc7fab996705ea8faf2b17784db9c1693183c
Olwethu516 <39149618+Olwethu516@users.noreply.github.com>
2026-05-18 18:56:02 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   base: '/oulasalon/',
-  plugins: [react()],
+  plugins: [
+    react({
+      jsxRuntime: 'automatic'
+    })
+  ]
 })
-
-
